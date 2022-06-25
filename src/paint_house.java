99326135d4b98cb0161187cd41f9406dc31af351
Bittere_Gift <329056266@qq.com>
2022-06-25 19:53:38 +0800

    更改了README

--- src/paint_house.java
+++ src/paint_house.java
@@ -2,7 +2,7 @@
  * @Author: Bittere_Gift 329056266@qq.com
  * @Date: 2022-06-25 19:06:53
  * @LastEditors: Bittere_Gift 329056266@qq.com
- * @LastEditTime: 2022-06-25 19:51:53
+ * @LastEditTime: 2022-06-25 19:53:10
  * @FilePath: \leetcode\src\paint_house.java
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * 
